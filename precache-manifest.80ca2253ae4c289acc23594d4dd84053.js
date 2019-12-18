132951f30f27b685a3a4e33dce50b7f4ab11845b
Snehangshu Bhattacharya <snehangshubhattacharya@yahoo.in>
2019-12-19 00:50:32 +0530

    Updates

---
+++ precache-manifest.80ca2253ae4c289acc23594d4dd84053.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d02ff2093c9a2f1ec929fabb8419bc4",
+    "url": "/react-github-pages/index.html"
+  },
+  {
+    "revision": "5e2ef7b7087a500a7090",
+    "url": "/react-github-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cb00de41366120efa94f",
+    "url": "/react-github-pages/static/js/2.78313752.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-github-pages/static/js/2.78313752.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e2ef7b7087a500a7090",
+    "url": "/react-github-pages/static/js/main.941c900e.chunk.js"
+  },
+  {
+    "revision": "cfeac05acd7c9c1ac7f2",
+    "url": "/react-github-pages/static/js/runtime-main.f4897514.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-github-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
